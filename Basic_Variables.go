c4ec5a9579a702289a1c2a50ab9dd9f125da5d0f
Pakpoom Rachtrachoo <33973448+G30RG3PAK@users.noreply.github.com>
2019-09-18 22:29:06 +0700

    Basic variable of Go lang and some syntax

---
+++ Basic_Variables.go
@@ -0,0 +1,20 @@
+package main
+import "fmt"
+func main()  {
+	var a = "Shibano" // var can declare one or more variables
+	fmt.Println(a)
+
+	var b,c int = 2019,123456 // var can declare one or more variables
+	fmt.Println(b, c)
+
+	var d = false
+	fmt.Println(d)
+
+	var n int  //declare without initialization is zero-valued
+	fmt.Println(n)
+
+	i := "Thanos" // := is shorthand for declaring and initializing a variable
+	fmt.Println(i)
+	
+	
+}
